b6efff7faf20c95e12765771e8ccb9365778c918
Pepeu 123 <pepeu2322@gmail.com>
2025-11-27 21:49:37 -0300

    Corrigindo senha do banco de dados

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ header("Content-Type: application/json; charset=UTF-8");
 $host = "ep-damp-sound-ado6c3f7-pooler.c-2.us-east-1.aws.neon.tech";
 $db_name = "neondb";
 $user = "neondb_owner";
-$password = "npg_8E6ccUHIaxAs";
+$password = "npg_8E6cCUhIaxAs";
 $port = "5432"; 
 
 // String de Conexão (DSN) para PostgreSQL
